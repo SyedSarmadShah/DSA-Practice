b5ccce98dcbfded4898da5f4cbc65400f9f50086
Syed Sarmad Shah <syedsarmadshah2225@gmail.com>
2025-10-11 23:14:35 +0500

    Added the Insert At Head function which is not working now

--- LinkedLists/singlyLinkedList.cpp
+++ LinkedLists/singlyLinkedList.cpp
@@ -5,12 +5,33 @@ class Node{
     public:
     int data;
     Node* next;
+
+    Node(){
+    this -> data = 0;
+    this -> next = NULL;
+}
+
 };
+
+void insertAtHead(Node*& head, int data) {
+    Node* temp = new Node(data);
+    temp -> next = head;
+    head = temp;
+}
 
 int main()
 {
-Node* node1 = new Node();
-cout << node1 -> data << endl ;
-cout << node1 -> next << endl ;
-return 0;
+    Node* head = NULL;
+    insertAtHead(head, 10);
+    insertAtHead(head, 20);
+    insertAtHead(head, 30);
+
+    Node* current = head;
+    while (current != NULL) {
+        cout << current -> data << " ";
+        current = current -> next;
+    }
+    cout << endl;
+
+    return 0;
 }
