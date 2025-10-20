788430fd49c0e54b5aa61336750bbb9547b00b3c
Syed Sarmad Shah <syedsarmadshah2225@gmail.com>
2025-10-20 21:46:41 +0500

    Added the function of adding in the middle

--- LinkedLists/singlyLinkedList.cpp
+++ LinkedLists/singlyLinkedList.cpp
@@ -1,49 +1,86 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
-    public:
+class Node {
+public:
     int data;
     Node* next;
 
-    Node(int data){
-    this -> data = data;
-    this -> next = NULL;
-}
-
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
 };
 
+// Function Prototypes
+void insertAtHead(Node*& head, int data);
+void insertAtPosition(Node*& head, int position, int data);
+void insertAtTail(Node*& tail, int data);
+void print(Node*& head);
+
+// Function Definitions
 void insertAtHead(Node*& head, int data) {
     Node* temp = new Node(data);
-    temp -> next = head;
+    temp->next = head;
     head = temp;
+}
+
+void insertAtPosition(Node*& head, int position, int data) {
+    if (position == 1) {
+        insertAtHead(head, data);
+        return;
+    }
+
+    Node* temp = head;
+    int cnt = 1;
+
+    // Traverse to the node before the desired position
+    while (cnt < position - 1 && temp != NULL) {
+        temp = temp->next;
+        cnt++;
+    }
+
+    if (temp == NULL) {
+        cout << "Position out of range!" << endl;
+        return;
+    }
+
+    Node* nodeToInsert = new Node(data);
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
 }
 
 void insertAtTail(Node*& tail, int data) {
     Node* temp = new Node(data);
-    tail -> next = temp;
-    tail = tail -> next;
+    tail->next = temp;
+    tail = temp;
 }
 
-void print(Node* &head) {
+void print(Node*& head) {
     Node* temp = head;
     while (temp != NULL) {
-        cout << temp -> data << " ";
-        temp = temp -> next;
+        cout << temp->data << " ";
+        temp = temp->next;
     }
     cout << endl;
 }
-int main()
-{
+
+int main() {
+    // Start with one node
     Node* node1 = new Node(10);
     Node* head = node1;
-    print(head);
     Node* tail = node1;
+
+    print(head);
 
     insertAtHead(head, 12);
     print(head);
-    
 
+    insertAtTail(tail, 15);
+    print(head);
+
+    insertAtPosition(head, 2, 20);
+    print(head);
 
     return 0;
 }
